155a2811aa3057819c5769e875a8b6dd1d63f660
vatergo <vatergo@mail.ru>
2020-09-26 16:34:14 +0500

    апи для погоды

--- server/routes/weather.js
+++ server/routes/weather.js
@@ -1,12 +1,33 @@
 import { Router } from 'express';
+import axios from 'axios';
 
 const router = Router();
 
 router.get('/', (req, res) => {
-    //https://www.metaweather.com/api/location/search/?lattlong=55.75,37.6167 woeid города из первого запроса во второй
-    //https://www.metaweather.com/api/location/2122265/
-    console.log(req.query);
-    res.json(req.query);
+    const { lattlong } = req.query;
+    axios.get(`https://www.metaweather.com/api/location/search/?lattlong=${lattlong}`)
+        .then(({ data }) => {
+            const currentGeo = data[0];
+            axios.get(`https://www.metaweather.com/api/location/${currentGeo.woeid}`)
+                .then(({ data }) => res.json({
+                    weather: data.consolidated_weather,
+                    locationType: data.location_type,
+                    title: data.title,
+                    time: data.time,
+                }))
+                .catch((er) => {
+                    console.error(er);
+                    res.status(500).json({
+                        message: `Ошибка получения погоды в ${currentGeo.title}`
+                    });
+                })
+        })
+        .catch((er) => {
+            console.error(er);
+            res.status(500).json({
+                message: 'Ошибка получения информации о местоположении'
+            });
+        })
 });
 
 
