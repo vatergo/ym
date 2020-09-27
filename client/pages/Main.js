d17289fac74dffc653e88f73151e9f2760eab1e4
vatergo <vatergo@mail.ru>
2020-09-27 15:07:07 +0500

    timeout

--- client/pages/Main.js
+++ client/pages/Main.js
@@ -85,10 +85,9 @@ function Main(props) {
                 });
         }, geoError, {
             enableHighAccuracy: true,
-            timeout: 5000,
+            timeout: 10000,
             maximumAge: 0
         });
-
     }
 
     return (
